b6ac5f34414a8d66c4c83462dd304f679c60aba8
Aashish paryani <aashishparyani7@gmail.com>
2020-05-22 20:19:12 +0530

    commiting first file app.py

---
+++ app.py
@@ -0,0 +1,27 @@
+thisList = ["Aashish","Vishal","Manish","Rajesh"]
+thisList.append("Gaurav")
+
+print(thisList[2:])
+
+print(thisList[:100])
+
+thisList[0] = "krishna"
+print(thisList)
+
+thisList.append("piku")
+print(thisList)
+
+thisList.insert(4,"satish")
+print(thisList)
+
+
+thisList.remove("Manish")
+print(thisList)
+
+thisList.pop()
+print(thisList)
+thisList.pop(2)
+print(thisList)
+
+
+
